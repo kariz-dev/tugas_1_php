1a7f35da61e702250d849e32654da77a09fab6ee
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2020-06-21 23:36:26 +0700

    tugas 1-2 php

---
+++ tugas2.php
@@ -0,0 +1,43 @@
+<?php
+    $cireng = "Rp1.000";
+    $esDuren = "Rp15.000";
+    $milo = "Rp20.000";
+    $esTeh = "Rp5.000";
+    $piscok = "Rp1.000";
+
+
+echo "<h1>DAFTAR MENU</h1>";
+
+echo "<table border=1>
+        <tr>
+            <th>No</th>
+            <th>Nama Menu</th>
+            <th>Harga</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>Cireng</th>
+            <th>$cireng</th>
+        </tr>
+        <tr>
+            <th>2</th>
+            <th>Es Duren</th>
+            <th>$esDuren</th>
+        </tr>
+        <tr>
+            <th>3</th>
+            <th>Es Milo</th>
+            <th>$milo</th>
+        </tr>
+        <tr>
+            <th>4</th>
+            <th>Es Teh</th>
+            <th>$esTeh</th>
+        </tr>
+        <tr>
+            <th>5</th>
+            <th>Piscok</th>
+            <th>$piscok</th>
+        </tr>
+    </table>";
+?>
